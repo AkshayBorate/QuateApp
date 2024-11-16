647b3ac57a20ed48e6d55b9cc465cc79a1ddd92d
Akshay Borate <akshayborate2002@gmail.com>
2024-11-17 00:14:47 +0530

    First-commit

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+document.getElementById("btn").addEventListener("click", function () {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function () {
+        console.log("ReadyState:", xmlhttp.readyState);
+        console.log("Status:", xmlhttp.status);
+        
+        if (xmlhttp.readyState == 4 && xmlhttp.status ==     200) {
+            var res = JSON.parse(xmlhttp.responseText);
+
+            if (res) {
+                var type = res.type;
+                var quote = res.quote;
+                document.getElementById("quote").innerHTML = `
+                <p>Quote - ${quote}</p>
+                <p>Type - ${type}</p>
+               `;
+            } else {
+                document.getElementById("quote").innerHTML = `<p>No Quotes are available</p>`;
+            }
+        } else if (xmlhttp.readyState === 4) {
+            document.getElementById("quote").innerHTML = `<p>Unable to fetch data</p>`;
+        }
+    };
+
+    xmlhttp.open("GET", "https://hindi-quotes.vercel.app/random", true);
+    xmlhttp.send();
+});
